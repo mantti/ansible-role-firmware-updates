0dfd3a89359d7f2f22f6c4ebd42f3437f2a47c1f
Mika Anttila <mika.anttila@tuni.fi>
2019-11-27 17:11:35 +0200

    This version works also without Mellanox OFED

--- files/check_ib_fw.sh
+++ files/check_ib_fw.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ibstat|awk -F ": " -v ORS="" '/Firmware version:/ {print $2}'
+#ibstat|awk -F ": " -v ORS="" '/Firmware version:/ {print $2}'
+mstflint -d `lspci |grep -i mellanox |cut -d " " -f1` q|awk -F ":" -v ORS="" '/FW Version/{gsub (" ", "", $2); print $2}'
